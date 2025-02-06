5c35639e69e9b917208586cc631be91958aeeba2
dasillyneko <jenaaryaman@gmail.com>
2025-02-06 19:17:18 +0530

    Initial Commit

---
+++ src/components/global/navbar.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+type Props = {}
+
+const Navbar =async (props: Props) => {
+  return (
+    <header className="fixed right-0 left-0 top-0 py-4 px-4 bg-black/40"></header>
+  )
+}
+
+
+export default Navbar
